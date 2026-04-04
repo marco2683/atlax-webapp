54e49da4e1e5840618f55559b38528473606aba5
marco2683 <sebastianimarco83@gmail.com>
2026-04-04 14:08:33 +1100

    Fix Netlify Functions Node ES module syntax error

--- webapp/netlify/functions/create-portal.js
+++ webapp/netlify/functions/create-portal.js
@@ -1,6 +1,7 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const { createClient } = require('@supabase/supabase-js');
+import Stripe from 'stripe';
+import { createClient } from '@supabase/supabase-js';
 
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 const supabaseUrl = process.env.VITE_SUPABASE_URL;
 const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 const supabase = createClient(supabaseUrl, supabaseServiceRoleKey);
